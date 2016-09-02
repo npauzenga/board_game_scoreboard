fd2acd4cfb1f95c4eff71546eaea1dc79daffad4
Nate <npauzenga@gmail.com>
2016-09-02 15:08:51 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import Scoreboard from './components/Scoreboard'
+
+// Styles
+import './styles/app.css'
+
+ReactDOM.render(<Scoreboard />, document.getElementById('container'));
